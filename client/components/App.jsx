b1789855a91f182f532ed3bd4ba52b28ec48e2f8
Eric Nague <ennague@gmail.com>
2018-05-31 08:42:56 -0700

    about to put into github organization

---
+++ client/components/App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDOM from ""
+
+class App extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state ={
+
+		}
+	}
+
+	render() {
+		return(
+			<p>hi from App</p>
+		)
+	}
+
+}
